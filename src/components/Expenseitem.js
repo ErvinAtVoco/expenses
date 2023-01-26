4f718233ab72abe8c858f8c3234eafeeb4517f97
ErvinAtVoco <ervin.tombak@voco.ee>
2023-01-26 12:04:05 +0200

    Esimene commit

---
+++ src/components/Expenseitem.js
@@ -0,0 +1,17 @@
+import './Expenseitem.css'
+import ExpenseDate from './ExpenseDate';
+function ExpenseItem(props) {
+    console.log(props)
+
+    return (
+        <div className="expense-item">
+            <ExpenseDate date={props.expenseData.date}></ExpenseDate>
+            <div className="expense-item__description">
+                <h2>{props.expenseData.title}</h2>
+                <div className="expense-item__price">{props.expenseData.price}</div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem
